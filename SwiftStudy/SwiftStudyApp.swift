68b9f2c82787a90df113efecb0e405605d923c4a
Juan Lucas Rossi <juan.lucas.rossi@gmail.com>
2023-09-08 16:24:51 -0300

    Initial Commit

---
+++ SwiftStudy/SwiftStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudyApp.swift
+//  SwiftStudy
+//
+//  Created by Juan Lucas Rossi on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
